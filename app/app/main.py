7e99ce510c8cd02f678ba11f3833a06f7cfe00e3
Nikolay Marusov <nmarusov@gmail.com>
2021-02-08 11:15:31 +0300

    Добавлено отслеживание ошибок с помощью Sentry.

--- app/app/main.py
+++ app/app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+import sentry_sdk
 
 from app.api.api_v1.api import api_router
 from app.core.config import settings
 
+
+sentry_sdk.init(settings.SENTRY_DSN, traces_sample_rate=1.0)
 
 app = FastAPI(
     title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
